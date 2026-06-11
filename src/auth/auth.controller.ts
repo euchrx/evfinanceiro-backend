a6dfa4435dcb12f8e89eac020f418a6488b84642
Christian Evangelista <chriismatos@icloud.com>
2026-06-11 03:27:44 -0300

    backend: 0.4

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,12 +10,18 @@ import {
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(
     private readonly authService: AuthService,
-  ) {}
+  ) { }
+
+  @Post('register')
+  register(@Body() dto: RegisterDto) {
+    return this.authService.register(dto);
+  }
 
   @Post('login')
   login(
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { compare } from 'bcrypt';
+import { compare, hash } from 'bcrypt';
 
 import { PrismaService } from '../prisma/prisma.service';
 import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
 export class AuthService {
@@ -61,4 +62,41 @@ export class AuthService {
       },
     });
   }
+
+  async register(dto: RegisterDto) {
+  const existingUser = await this.prisma.user.findUnique({
+    where: {
+      email: dto.email,
+    },
+  });
+
+  if (existingUser) {
+    throw new ConflictException('Este e-mail já está em uso.');
+  }
+
+  const passwordHash = await hash(dto.password, 10);
+
+  const user = await this.prisma.user.create({
+    data: {
+      name: dto.name,
+      email: dto.email,
+      passwordHash,
+      role: 'ADMIN',
+    },
+  });
+
+  const accessToken = await this.jwtService.signAsync({
+    sub: user.id,
+    email: user.email,
+  });
+
+  return {
+    accessToken,
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    },
+  };
+}
 }
